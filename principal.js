130bd48ce97643b33a7b328846ba4205212a755e
Luiz Gomes <luizfilho85@yahoo.com.br>
2015-11-14 03:43:14 -0300

    Realizado mudanças de tempo do banner

--- principal.js
+++ principal.js
@@ -4,4 +4,4 @@ function trocaBanner() {
 	bannerAtual = (bannerAtual + 1) % 2;
 	document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
 }
-setInterval(trocaBanner, 2000);
+setInterval(trocaBanner, 1000);
