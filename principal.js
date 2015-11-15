e4c3f3ba7a83b9ce64110ea634db612553f7070b
Luiz Gomes <luizfilho85@yahoo.com.br>
2015-11-14 22:55:44 -0300

    Alterando textos do banner

---
+++ principal.js
@@ -0,0 +1,7 @@
+var banners = ["Do lixo ao luxo!", "Reaproveitar é aproveitar!"];
+var bannerAtual = 0;
+function trocaBanner() {
+	bannerAtual = (bannerAtual + 1) % 2;
+	document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
+}
+setInterval(trocaBanner, 3000);
