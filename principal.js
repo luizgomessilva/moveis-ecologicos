fdc6c983ee2cb63e467d7cd9d0f36de2a7e8be55
Luiz Gomes <luizfilho85@yahoo.com.br>
2015-11-16 21:33:10 -0300

    Aumento do tempo do banner test

--- principal.js
+++ principal.js
@@ -4,4 +4,4 @@ function trocaBanner() {
 	bannerAtual = (bannerAtual + 1) % 2;
 	document.querySelector('h2#mensagem').textContent = banners[bannerAtual];
 }
-setInterval(trocaBanner, 3000);
+setInterval(trocaBanner, 3500);
